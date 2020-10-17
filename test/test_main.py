df06cb83a8cf349d0030e60590bb7b2421d15270
yasuaki-miyoshi <ikusay.443@gmail.com>
2020-10-17 16:52:29 +0900

    Add test_main.py file

---
+++ test/test_main.py
@@ -0,0 +1,10 @@
+"""Entry point of unit test."""
+# Copyright (c) 2020 Yasuaki Miyoshi.
+#
+# This software is released under the MIT License.
+# see http://opensource.org/licenses/mit-license.php
+
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
