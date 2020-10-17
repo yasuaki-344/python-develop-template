148b1778dd2fcecf2532d6c85db45337dfd58dae
yasuaki-miyoshi <ikusay.443@gmail.com>
2020-10-17 16:22:42 +0900

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+"""Entry point of application"""
+# Copyright (c) 2020 Yasuaki Miyoshi.
+#
+# This software is released under the MIT License.
+# see http://opensource.org/licenses/mit-license.php
+
+if __name__ == "__main__":
+    print("hello world")
