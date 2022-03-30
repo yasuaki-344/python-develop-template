6e338c8628adcc623ba95961ba73c096a0e089ae
yasuaki-344 <ikausay.443@gmail.com>
2022-03-30 21:56:42 +0900

    Create github_user_info_extractor.py

---
+++ src/business_logics/github_user_info_extractor.py
@@ -0,0 +1,6 @@
+class GitHubUserInfoExtractor:
+    def __init__(self) -> None:
+        pass
+
+    def extract_user_info(self, json):
+        return json
--- src/use_cases/github_user_interactor.py
+++ src/use_cases/github_user_interactor.py
@@ -1,13 +1,16 @@
 """example usecase class implementation"""
+from business_logics.github_user_info_extractor import GitHubUserInfoExtractor
 from repositories.giithub_user_repository import GitHubUserRepository
 
 
 class GitHubUserInteractor:
     """This is just example"""
 
-    def __init__(self, repository: GitHubUserRepository):
+    def __init__(self, repository: GitHubUserRepository, business_logic: GitHubUserInfoExtractor):
         self.__repository = repository
+        self.__business_logic = business_logic
 
     def get_user_information(self):
         res = self.__repository.get_user_info("defunkt")
-        print(res)
+        info = self.__business_logic.extract_user_info(res)
+        print(info)
